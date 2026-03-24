91f535d0598a50ae1d70114600f667d3dd8a78e7
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-03-24 16:50:03 +0400

    feat: home planning_slide

--- src/components/public/planning_slide.jsx
+++ src/components/public/planning_slide.jsx
@@ -4,18 +4,27 @@ import React from "react";
 import { useGetTanstack } from "@/hooks/useTanstack";
 import { useQueryState } from "nuqs";
 import { useRouter } from "@/i18n/routing";
-import { Box, Flex, For, Image, Skeleton } from "@chakra-ui/react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Autoplay, Pagination } from "swiper/modules";
-// import "swiper/css";
-// import 'swiper/css/effect-coverflow';
-// import "swiper/css/pagination";
+import {
+  Box,
+  Flex,
+  For,
+  Image,
+  Skeleton,
+  Stack,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const PlanningSlide = () => {
   const router = useRouter();
 
   const { isLoading, data } = useGetTanstack("templates");
   const [_, setTemplate] = useQueryState("template");
+
+  const [isLaptop] = useMediaQuery("(max-width: 992px)");
 
   const handleSelect = (id) => {
     setTemplate(id);
@@ -37,103 +46,45 @@ export const PlanningSlide = () => {
   }
 
   return (
-    <Flex w="100%" gap="16px" justify={"center"}>
+    <Swiper
+      slidesPerView={isLaptop ? 2 : 3}
+      // slidesPerView={isMobile ? 1 : isLaptop ? 2 : 3}
+      spaceBetween={16}
+      speed={500}
+      loop={true}
+      pagination={true}
+      modules={[Pagination]}
+    >
       {data?.map(({ id, templateImageMobile }) => {
         return (
-          <Box
-            cursor={"pointer"}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            // w="100%"
-            // h="100%"
-            w={{ base: "168px", md: "286px" }}
-            h={{ base: "268px", md: "457px" }}
-            // w="286px"
-            // h="457px"
-            borderRadius={"25px"}
-            overflow={"hidden"}
+          <SwiperSlide
+            key={id}
             onClick={() => handleSelect(id)}
           >
-            <Image
-              src={templateImageMobile}
-              alt={`Template ${id}`}
-              // w="286px"
-              // h="457px"
-              w="100%"
-              h="100%"
-              objectFit="cover"
-              transition="all 0.3s ease"
-              borderRadius={"25px"}
-              _hover={{
-                transform: "scale(1.1)",
-              }}
-            />
-          </Box>
+            <Stack
+              cursor={"pointer"}
+              w={"100%"}
+              h={{ base: "268px", sm: "457px" }}
+              alignItems={"center"}
+              justifyContent={"Center"}
+              overflow={"hidden"}
+            >
+              <Image
+                src={templateImageMobile}
+                alt={`Template ${id}`}
+                w={{ base: "168px", sm: "286px" }}
+                h={{ base: "268px", sm: "457px" }}
+                objectFit="contain"
+                transition="all 0.3s ease"
+                borderRadius={"25px"}
+                _hover={{
+                  transform: "scale(1.1)",
+                }}
+              />
+            </Stack>
+          </SwiperSlide>
         );
       })}
-    </Flex>
+    </Swiper>
   );
 };
-
-{
-  /* <Swiper
-  // effect={"coverflow"}
-  // grabCursor={true}
-  // centeredSlides={true}
-  slidesPerView={3}
-  // coverflowEffect={{
-  //   rotate: 15,
-  //   stretch: 0,
-  //   depth: 80,
-  //   modifier: 1,
-  //   slideShadows: false,
-  // }}
-  spaceBetween={16}
-  speed={500}
-  loop={true}
-  pagination={true}
-  // autoplay={{
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // }}
-  modules={[Autoplay, Pagination]}
->
-  {data?.map(({ id, templateImageMobile }) => {
-    return (
-      <SwiperSlide key={id} onClick={() => handleSelect(id)} className="slider">
-        <Box
-          cursor={"pointer"}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          // w="100%"
-          // h="100%"
-          w="286px"
-          h="457px"
-          borderRadius={"25px"}
-          overflow={"hidden"}
-        >
-          <Image
-            src={templateImageMobile}
-            alt={`Template ${id}`}
-            // w="286px"
-            // h="457px"
-            w="100%"
-            h="100%"
-            objectFit="cover"
-            transition="all 0.3s ease"
-            borderRadius={"25px"}
-            _hover={{
-              transform: "scale(1.1)",
-            }}
-          />
-        </Box>
-      </SwiperSlide>
-    );
-  })}
-<SwiperSlide className="slider">
-          <Box w="286px" h="457px" bg="#D9D9D9" borderRadius="25px" />
-        </SwiperSlide>
-</Swiper>; */
-}
